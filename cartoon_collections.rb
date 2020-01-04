0ed15d50b14671575fbb5a9bef5c70e12f273d7c
Oliver Dahl <oliverpdahl@gmail.com>
2020-01-04 15:34:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,8 +3,10 @@ def roll_call_dwarves(array) #Takes in the array of the names of the dwarves
   array.each_with_index {|name, index| puts "#{index + 1} #{name}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(calls) #input is an array of calls to it
+  #using regex and collect to change each of them and .gsub
+  calls.collect do |call|
+    call.gsub
 end
 
 def long_planeteer_calls# code an argument here
