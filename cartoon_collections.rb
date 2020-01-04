a953cc362c1cefbaa84f08fb860ed362e2195ba0
Oliver Dahl <oliverpdahl@gmail.com>
2020-01-04 15:28:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,6 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array) #Takes in the array of the names of the dwarves
+  #Outputs each of the names in number order using each with index
+  array.each_with_index {|name, index| puts "#{index + 1} #{name}
 end
 
 def summon_captain_planet# code an argument here
